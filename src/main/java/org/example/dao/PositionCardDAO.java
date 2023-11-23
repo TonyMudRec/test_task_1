dd793349a2764bcc26159f437ebade4e7e951f42
mudrec <sm1le88man@gmail.com>
2023-11-23 12:06:19 +0300

    added getName function

--- src/main/java/org/example/dao/PositionCardDAO.java
+++ src/main/java/org/example/dao/PositionCardDAO.java
@@ -1,5 +1,31 @@
 package org.example.dao;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class PositionCardDAO extends BaseDAO {
+
+    private Connection connection;
+
+    public PositionCardDAO(Connection conn) {
+        connection = conn;
+    }
+
+    public Optional<String> getName(Long groupId) throws SQLException {
+        String sql = "SELECT pc.name " +
+                "FROM position_card pc " +
+                "WHERE pc.groupId = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setLong(1, groupId);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(resultSet.getString("name"));
+            }
+        }
+        return Optional.empty();
+    }
 }
